1e13c00f2d8ce49d16951c4f5cc4378ac80309ec
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-03-27 11:41:16 -0300

    feat: create terms

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,18 +2,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout () {
     return (
-        <Stack screenOptions={{headerShown: true,
-        headerStyle: {
-            backgroundColor: "#52057B",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-            fontWeight: "bold",
-        }
-
-        }}>
+        <Stack>
             <Stack.Screen name="index" options={{ title: "Home" }} />
             <Stack.Screen name="sobre" options={{ title: "Sobre" }} />
+            <Stack.Screen name="(aux)/termos" options={{ title: "Termos de uso" }} />
         </Stack>    
     )
 }
